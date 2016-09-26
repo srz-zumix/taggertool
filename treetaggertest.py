9b9f378b924962006dbc55d8d7eee29120376bd2
srz_zumix <zumix.cpp@gmail.com>
2016-09-27 08:17:52 +0900

    add treetagger test

---
+++ treetaggertest.py
@@ -0,0 +1,8 @@
+﻿import treetaggerwrapper
+import os
+
+tagdir = os.getenv('TREETAGGER_ROOT')
+tagger = treetaggerwrapper.TreeTagger(TAGLANG='en',TAGDIR=tagdir)
+tags = tagger.TagText(u"Save the time of the reader. SAMPLETEST. SampleTest.")
+for tag in tags:
+    print tag
