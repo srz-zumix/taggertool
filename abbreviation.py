435c63878af5edf3353ad9e48031e04554164e61
srz_zumix <zumix.cpp@gmail.com>
2016-10-04 22:19:57 +0900

    codewordranker: support exclude abbreviation: update

--- abbreviation.py
+++ abbreviation.py
@@ -5,10 +5,20 @@ import os
 import sys
 import re
 import codecs
+import treetaggerwrapper
+
+import keywords
 
 from argparse import ArgumentParser
 
-has_error = False
+tagdir = os.getenv('TREETAGGER_ROOT')
+tagger = treetaggerwrapper.TreeTagger(TAGLANG='en',TAGDIR=tagdir)
+options = None
+
+whitelist = []
+gene = []
+langkeywords = []
+
 
 def parse_command_line():
     parser = ArgumentParser()
@@ -28,23 +38,127 @@ def parse_command_line():
     return options, parser
 
 
-def checktagger(text):
+r_alnum = re.compile(r'^[a-zA-Z0-9]+$')
+def isalnum(s):
+    return r_alnum.match(s) is not None
+
+
+r_alpha = re.compile(r'^[a-zA-Z]+$')
+def isalpha(s):
+    return r_alpha.match(s) is not None
+
+
+r_block_comment_begin = re.compile('(.*)/\*.*')
+r_block_comment_end = re.compile('.*\*/(.*)')
+line_comment = '//'
+
+
+def checkcomment(text, block_comment):
+    if block_comment:
+        m = r_block_comment_end.match(text)
+        if m:
+            block_comment = False
+            text = m.group(1)
+    if not block_comment:
+        m = r_block_comment_begin.match(text)
+        if m:
+            block_comment = True
+            text = m.group(1)
+        line_comment_start = text.find(line_comment)
+        if line_comment_start != -1:
+            text = text[:line_comment_start]
+    return text, block_comment
+
+
+def is_whitelist(word):
+    if word in whitelist:
+        return True
+    if word in langkeywords:
+        return True
+    # 辞書にあったら除外
+    if word in gene:
+        return True
+    return False
+
+
+def is_suspicion(word):
+    length = len(word)
+    # 1文字だけは除外
+    if length <= 1:
+        return False
+    if not isalpha(word):
+        return False
+    if is_whitelist(word):
+        return False
+    # 過去形
+    if length > 2 and word.endswith('ed'):
+        if is_whitelist(word[:-2]):
+            return False
+        if is_whitelist(word[:-1]):
+            return False
+    # 進行形
+    if length > 3 and word.endswith('ing'):
+        if is_whitelist(word[:-3]):
+            return False
+        if is_whitelist(word[:-3] + 'e'):
+            return False
+    # 複数形
+    if word.endswith('s'):
+        if is_whitelist(word[:-1]):
+            return False
+    if length > 3 and word.endswith('ies'):
+        if is_whitelist(word[:-3] + 'y'):
+            return False
+    return True
+
+
+r_sign = re.compile('[!-/:-@[-`{-~]')
+def text_transform(text):
+    text = re.sub("([a-z0-9])([A-Z])(?=[a-z])", lambda x: x.group(1) + "_" + x.group(2), text)
+    return re.sub(r_sign, ' ', text)
+
+
+def checktagger(text, line, words):
+    tags_plain = tagger.TagText(text)
+    tags = treetaggerwrapper.make_tags(tags_plain)
+    for tag in tags:
+        if isinstance(tag, treetaggerwrapper.NotTag):
+            continue
+        word = tag.word.lower()
+        if is_suspicion(word):
+            if words.has_key(word):
+                words[word].append(line)
+            else:
+                words[word] = [line]
 
 
 def check(filepath):
     filename = os.path.basename(filepath)
     f = codecs.open(filepath, 'r', encoding='utf-8-sig')
-    count = 0
+    line_count = 1
+    block_comment = False
+    words = {}
     for line in f:
-        print(str(count) + ':' + line)
         text = line.strip()
-        if len(text) > 0 and not text.startswith('//'):
-            checktagger(text)
-        count += 1
+        text, block_comment = checkcomment(text, block_comment)
+        if not block_comment:
+            if len(text) > 0:
+                checktagger(text_transform(text), line_count, words)
+        line_count += 1
+    return words
+
+
+def printresult(filepath, d):
+    for k,v in sorted(d.items(), key=lambda x: len(x[1])):
+        for line in v:
+            print("{0}({1}): warning: \"{2}\": abbreviation ??".format(filepath, line, k))
 
 
 def checkfile(f):
-    check(f)
+    global langkeywords
+    langkeywords = keywords.getkeywords(f)
+    words = check(f)
+    printresult(f, words)
 
 
 def checkdir(dir):
@@ -54,6 +168,7 @@ def checkdir(dir):
             checkdir(d)
         else:
             checkfile(d)
+
 
 def main():
     options, parser = parse_command_line()
@@ -66,5 +181,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    if has_error:
-        sys.exit(1)
--- codewordranker.py
+++ codewordranker.py
@@ -6,12 +6,17 @@ import sys
 import re
 import codecs
 import treetaggerwrapper
+import keywords
 
 from argparse import ArgumentParser
 
 tagdir = os.getenv('TREETAGGER_ROOT')
 tagger = treetaggerwrapper.TreeTagger(TAGLANG='en',TAGDIR=tagdir)
 options = None
+
+exclude = []
+exclude_keywords = []
+
 
 def parse_command_line():
     parser = ArgumentParser()
@@ -25,6 +30,16 @@ def parse_command_line():
         '--html',
         action='store_true',
         help='output html'
+    )
+    parser.add_argument(
+        '--exclude_keywords',
+        action='store_true',
+        help='exclude language keywords'
+    )
+    parser.add_argument(
+        '--exclude',
+        action='append',
+        help='exlude word'
     )
     parser.add_argument(
         'file',
@@ -46,13 +61,26 @@ def isalpha(s):
     return r_alpha.match(s) is not None
 
 
+r_sign = re.compile('[!-/:-@[-`{-~]')
+def text_transform(text):
+    return re.sub(r_sign, ' ', text)
+
+
 def checkword(word):
+    if len(word) <= 1:
+        return False
     if not isalpha(word):
+        return False
+    if word in exclude:
+        return False
+    if word in exclude_keywords:
         return False
     return True
 
 
 def checktagger(text, line, words):
+    # HogeFuga -> Hoge_Fuga
+    text = re.sub("([a-z0-9])([A-Z])(?=[a-z])", lambda x: x.group(1) + "_" + x.group(2), text)
     tags_plain = tagger.TagText(text)
 #    tags_plain = tagger.TagText(text, notagdns=True)
 #    for tag in tags_plain:
@@ -61,7 +89,7 @@ def checktagger(text, line, words):
     for tag in tags:
         if isinstance(tag, treetaggerwrapper.NotTag):
             continue
-        word = tag.lemma
+        word = tag.word.lower()
         if checkword(word):
             if words.has_key(word):
                 words[word].append(line)
@@ -85,12 +113,14 @@ def printhtml(words):
         return (num - minnum) * difffontsize / diff + minfontsize
     print('<html><body>')
     for k,v in sorted(words.items(), key=lambda x: x[0]):
-        print('<span style="font-size: {0}">{1}</span>'.format(calcfontsize(len(v)), k))
+        print('<span style="font-size: {0}px">{1}</span>'.format(calcfontsize(len(v)), k))
     print('</body></html>')
+
 
 r_block_comment_begin = re.compile('(.*)/\*.*')
 r_block_comment_end = re.compile('.*\*/(.*)')
 line_comment = '//'
+
 
 def checkcomment(text, block_comment):
     if block_comment:
@@ -106,6 +136,7 @@ def checkcomment(text, block_comment):
         line_comment_start = text.find(line_comment)
         if line_comment_start != -1:
             text = text[:line_comment_start]
+    return text, block_comment
 
 
 def check(filepath):
@@ -117,10 +148,10 @@ def check(filepath):
     for line in f:
         #print(str(count) + ':' + line)
         text = line.strip()
-        checkcomment(text, block_comment)
+        text, block_comment = checkcomment(text, block_comment)
         if not block_comment:
-            if len(text) > 0 and not text.startswith('//'):
-                checktagger(text, line_count, words)
+            if len(text) > 0:
+                checktagger(text_transform(text), line_count, words)
         line_count += 1
     if options.html:
         printhtml(words)
@@ -129,6 +160,9 @@ def check(filepath):
 
 
 def checkfile(f):
+    global exclude_keywords
+    if options.exclude_keywords:
+        exclude_keywords = keywords.getkeywords(f)
     check(f)
 
 
@@ -140,9 +174,14 @@ def checkdir(dir):
         else:
             checkfile(d)
 
+
 def main():
     global options
+    global exclude
     options, parser = parse_command_line()
+    if options.exclude:
+        for e in options.exclude:
+            exclude.extend(e.split(','))
     for f in options.file:
         if os.path.isdir(f):
             checkdir(f)
