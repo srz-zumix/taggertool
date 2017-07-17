ddf148b7488e834b8c4e6cd5124ad8e7a283b83b
srz_zumix <zumix.cpp@gmail.com>
2017-07-17 21:47:43 +0900

    update test

--- taggertools/tests/test_glosbe.py
+++ taggertools/tests/test_glosbe.py
@@ -72,6 +72,9 @@ class Test_glosbe(unittest.TestCase):
     def test_kpm(self):
         self.assertEqual(DictResult.Found, abbreviation_glosbe.check_suspicion('kpm'))
 
+    def test_electric(self):
+        self.assertEqual(DictResult.Found, abbreviation_glosbe.check_suspicion('electric'))
+
     def test_pine(self):
         self.assertNotEqual(DictResult.Abbreviation, abbreviation_glosbe.check_suspicion('pine'))
 
